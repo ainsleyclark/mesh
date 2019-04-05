2d86056f23d69a0689e0fe4ae3eb5d3baee53b70
Ainsley <ainsley@reddico.co.uk>
2019-04-05 17:55:32 +0100

    Changed to util

---
+++ demopages/includes/header.php
@@ -0,0 +1,28 @@
+<!-- =====================
+    Header
+    ===================== -->
+<header class="bgcolor-black">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 flex justify-content-between header">
+                <div class="header__logo flex justify-content-around align-items-center">
+                    <span class="type-3 color-primary type-weight-light">mesh</span>
+                    <a href="getstarted.html">Get Started</a>
+                    <a href="grid.html">Grid</a>
+                    <a href="components.html">Components</a>
+                </div>
+                <div class="header__icons flex align-items-center">
+                    <a href="https://www.linkedin.com/in/ainsleyclark/" target="_blank">
+                        <i class="fab fa-linkedin fa-2x color-white mx-3"></i>
+                    </a>
+                    <a href="https://github.com/ainsleyclark/mesh" target="_blank">
+                        <i class="fab fa-github fa-2x color-white mx-3"></i>
+                    </a>
+                    <a href="https://github.com/ainsleyclark/mesh" target="_blank">
+                        <button class="mx-3">Download</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ demopages/includes/nav.php
@@ -0,0 +1,35 @@
+<!-- =====================
+    Navigation
+    ===================== -->
+<nav class="nav">
+    <span class="nav__title type-3">Documentation</span>
+    <div class="divider bgcolor-white"></div>
+    <ul class="color-black ml-4">
+        <!-- Getting Started -->
+        <li>Getting Started</li>
+            <ul>
+                <li>Download</li>
+                <li>Contents</li>
+                <li>Variables</li>
+            </ul>
+        <!-- Utilities -->
+        <li>Utilities</li>
+        <ul>
+            <li>Grid</li>
+            <li>Flexbox</li>
+            <li>Position</li>
+            <li>Sizing</li>
+            <li>Colors</li>
+            <li>Type</li>
+            <li>Media</li>
+        </ul>
+        <!-- Components -->
+        <li>Components</li>
+        <ul>
+            <li>Alert</li>
+            <li>Bellutton</li>
+            <li>Card</li>
+            <li>Modal</li>
+        </ul>
+    </ul>
+</nav>
