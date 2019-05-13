1f6308f4457ff897dbd829158b2c7450d15e53bb
Jake <jacobstefan92@gmail.com>
2019-05-13 15:23:35 -0400

    proofed front-page

--- meshwebsite/documentation/getting-started/index.php
+++ meshwebsite/documentation/getting-started/index.php
@@ -31,9 +31,9 @@ $tableOfContents = [
                 <!-- What is mesh? -->
                 <article class="section-scroll" id="whatIs">
                     <h2 class="pb-1 b-b-light mt-5 mb-1 hash">What is mesh?</h2>
-                    <p>mesh (pronounced /meʃ/) is an open source pure CSS framework for building websites for efficentley. Unlike other frameworks, you can use mesh without detracting from the unique visual signature of a website, whilst still being able to add components to quicken your workflow.</p>
-                    <p>It has a modular approach to a responsive grid system, you can define containers, rows and columns and set them indepdantly of screen width. Five breakpoints helps you sculpt your site to different screen sizes and a easy naming system makes it ideal for begginers or patrons of the web.</p>
-                    <p>mesh has huge amounts of utility classes to help you along the way, such as responsive text, responsive width & height classes, border helper classes & spacers for when margin isn't quite good enough, and alot lot more...</p>
+                    <p>mesh (pronounced /meʃ/) is an open source pure CSS framework for building websites for efficiently. Unlike other frameworks, you can use mesh without detracting from the unique visual signature of a website, whilst still being able to add components to accelerate your workflow.</p>
+                    <p>IL has a modular approach to a responsive layout system. You can define containers, rows and columns and set them independently of screen width. Five breakpoints let you sculpt your site to different screen sizes and an easy naming system makes it ideal for beginners or patrons of the web.</p>
+                    <p>mesh has huge amounts of utility classes to help you along the way. Responsive text, responsive width & height classes, border helper classes & spacers for when margin isn't quite good enough, and a lot lot more...</p>
                 </article>
 
                 <!-- Download -->
@@ -44,20 +44,20 @@ $tableOfContents = [
                         <div class="col-12">
                             <h3 class="c-primary">meshBuilder:</h3>
                             <p>meshBuilder is an extremley powerful tool allowing you to set variables before downloading the compiled CSS. You can adjust a vast amount of different variables to suit your needs, including but not limited to: 
-                                <br>Grid breakpoints, colors (and attributes), spacing sizes (padding & margin), position properties, font sizes and a lot more.</p>
-                            <p>Not only that, but you can choose to eliminate any utilities or components you may not use in your project. This ultimatley makes things extremley fast and your css becomes a lot less bloated without the need to download our scss files.</p>
+                                <br>Layout breakpoints, colors (and attributes), spacing sizes (padding & margin), position properties, font sizes and a lot more.</p>
+                            <p>Not only that, but you can choose to eliminate any utilities or components you may not use in your project. This ultimately makes things extremely fast and your css becomes much less bloated without the need to download our scss files.</p>
                             <button class="btn btn-secondary btn-rounded mt-1">Go to meshBuilder</button>
                         </div>
                     </div>
                     <div class="row mt-3">
                         <div class="col-12 col-tab-6">
                             <h3 class="c-primary">meshCSS:</h3>
-                            <p>This is the standard version that comes with minified and unminified versions of mesh, including mesh grid, which is a barebones grid with flex, order & display. Choose this option if you have no familairty with Sass.</p>
+                            <p>This is the standard version that comes with minified and unminified versions of mesh, including mesh layout, which is a barebones css file with flex, order & display. Choose this option if you have no familairty with Sass.</p>
                             <button class="btn btn-primary btn-rounded mt-1">Download meshCSS</button>
                         </div>
                         <div class="col-12 col-tab-6">
                             <h3 class="c-primary">meshSaas:</h3>
-                            <p>This is the uncompiled scss version of mesh, you need a way to compile Sass files if you choose this option, however, you will be able to modify the contents of mesh by change variables and various other components.</p>
+                            <p>This is the uncompiled scss version of mesh, you need a way to compile Sass files if you choose this option, however, you will be able to modify the contents of mesh by changing variables and various other components.</p>
                             <button class="btn btn-primary btn-rounded mt-1">Download meshSaas</button>
                         </div>
                     </div>
@@ -66,8 +66,8 @@ $tableOfContents = [
                 <!-- JS -->
                 <article class="section-scroll" id="js">
                     <h2 class="b-b-light hash">A note about JS</h2>
-                    <p>We realise everything can't be done in CSS & somethings are best done with JavaScript. Which is why we have included example usage with our specific JS components. This makes things easier if you are using a front-end framework or want to write your own code but if you dont fancy that, just use ours!</p>
-                    <p> Of course, if there is a CSS alternative we have also provided that too, mainly through the use of checkboxes to show & hide components.</p>
+                    <p>We realise everything can't be done in CSS & somethings are best done with JavaScript. Which is why we have included example usage with our specific JS components. This makes things easier if you are using a front-end framework or want to write your own code, but if you dont fancy that, just use ours!</p>
+                    <p> Of course, where there is a CSS alternative we have also provided that too, mainly through the use of checkboxes to show & hide components.</p>
                 </article>
 
                 <!-- Notate -->
