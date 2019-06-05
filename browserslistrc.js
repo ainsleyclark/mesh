48335eaf0c0460197812d8b6f35baf64fc0b6676
ainsleyclark <info@ainsleyclark.com>
2019-06-05 13:26:34 +0100

    Changed .browserlistrc to js file to use module exports, avoid conflict with master mesh-site

---
+++ browserslistrc.js
@@ -0,0 +1,12 @@
+//Browsers that we support
+
+module.exports = {
+	overrideBrowserslist: [
+        'last 2 versions',
+        'Chrome >= 35',
+        'Firefox >= 31',
+        'not ie 10',
+        'not ie_mob 10',
+        'Safari >= 9'
+    ],
+}
